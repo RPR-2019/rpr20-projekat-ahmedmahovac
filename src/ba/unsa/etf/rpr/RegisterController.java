93c83d9d5f24992cc1e385da91fabaf5faa2d17a
ahmedmahovac <amahovac1@etf.unsa.ba>
2021-03-12 05:43:40 +0100

    added tooltips to register window and improved validation

--- src/ba/unsa/etf/rpr/RegisterController.java
+++ src/ba/unsa/etf/rpr/RegisterController.java
@@ -4,10 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
@@ -20,12 +17,17 @@ public class RegisterController {
     public TextField fldLastName;
     public TextField fldFirstName;
     public PasswordField fldPassword;
+    public Tooltip ttEmail;
+    public Tooltip ttUsername;
+    public Tooltip ttLastName;
+    public Tooltip ttFirstName;
+    public Tooltip ttPassword;
 
     private boolean containsUppercase = true;
     private boolean containsNumbers = true;
     private boolean containsEasilyGuessed = false;
     private boolean containsEmptyFields = false;
-
+    private boolean containsFiveChars = true;
 
     public void actionLogin() {
         Parent root = null;
@@ -42,37 +44,59 @@ public class RegisterController {
     public void actionRegister(ActionEvent actionEvent) {
         // dodati provjeru unikatnosti emaila i usernamea
         boolean ok = true;
-        if(fldEmail.getText().trim().isBlank() || fldFirstName.getText().trim().isBlank() || fldLastName.getText().trim().isBlank() || fldUsername.getText().trim().isBlank() || fldPassword.getText().trim().isBlank()) {
-            containsEmptyFields = true;
-            ok = false;
-        }
-        if(!checkPassword(fldPassword.getText())) ok = false;
-        if(!ok) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error");
-            alert.setHeaderText("Entered text fields do not meet the requirements");
-            String content = new String("These requirements have not been satisfied: \n");
-                if(containsEasilyGuessed) content+="Password should not contain first name or last name \n";
-                if(!containsNumbers) content+="Password should contain at least one digit \n";
-                if(!containsUppercase) content+="Password should contain at least one uppercase character \n";
-                if(containsEmptyFields) content+="No field can remain empty \n";
-            alert.setContentText(content);
-            fldPassword.getStyleClass().removeAll("fieldOk");
-            fldUsername.getStyleClass().removeAll("fieldOk");
-            fldLastName.getStyleClass().removeAll("fieldOk");
-            fldFirstName.getStyleClass().removeAll("fieldOk");
-            fldEmail.getStyleClass().removeAll("fieldOk");
-            fldPassword.getStyleClass().add("fieldNotOk");
-            fldUsername.getStyleClass().add("fieldNotOk");
-            fldLastName.getStyleClass().add("fieldNotOk");
-            fldFirstName.getStyleClass().add("fieldNotOk");
-            fldEmail.getStyleClass().add("fieldNotOk");
-            alert.showAndWait();
-        }
-        else {
+
+
+            if(!checkPassword(fldPassword.getText())) {
+                ok = false;
+                fldPassword.getStyleClass().removeAll("fieldOk");
+                fldPassword.getStyleClass().add("fieldNotOk");
+            }
+            else {
+                fldPassword.getStyleClass().removeAll("fieldNotOk");
+                fldPassword.getStyleClass().add("fieldOk");
+            }
+            // dodaj i da je unique u bazi email i username
+            if(fldUsername.getText().trim().isBlank()) {
+                ok = false;
+                fldUsername.getStyleClass().removeAll("fieldOk");
+                fldUsername.getStyleClass().add("fieldNotOk");
+            }
+            else {
+                fldUsername.getStyleClass().removeAll("fieldNotOk");
+                fldUsername.getStyleClass().add("fieldOk");
+            }
+            if(fldLastName.getText().trim().isBlank()) {
+                ok = false;
+                fldLastName.getStyleClass().removeAll("fieldOk");
+                fldLastName.getStyleClass().add("fieldNotOk");
+            }
+            else {
+                fldLastName.getStyleClass().removeAll("fieldNotOk");
+                fldLastName.getStyleClass().add("fieldOk");
+            }
+            if(fldFirstName.getText().trim().isBlank()) {
+                ok = false;
+                fldFirstName.getStyleClass().removeAll("fieldOk");
+                fldFirstName.getStyleClass().add("fieldNotOk");
+            }
+            else {
+                fldFirstName.getStyleClass().removeAll("fieldNotOk");
+                fldFirstName.getStyleClass().add("fieldOk");
+            }
+            if(fldEmail.getText().trim().isBlank()) {
+                ok = false;
+                fldEmail.getStyleClass().removeAll("fieldOk");
+                fldEmail.getStyleClass().add("fieldNotOk");
+            }
+            else {
+                fldEmail.getStyleClass().removeAll("fieldNotOk");
+                fldEmail.getStyleClass().add("fieldOk");
+            }
+        if(ok) {
             // dodaj u bazi
             // bolje je dodat neki context text uz field koji je pogresan a ne otvarat novi prozor al nek zasad stoji da sva polja pocrvene
-            fldPassword.getStyleClass().removeAll("fieldNotOk");
+
+            /*fldPassword.getStyleClass().removeAll("fieldNotOk");
             fldUsername.getStyleClass().removeAll("fieldNotOk");
             fldLastName.getStyleClass().removeAll("fieldNotOk");
             fldFirstName.getStyleClass().removeAll("fieldNotOk");
@@ -82,14 +106,25 @@ public class RegisterController {
             fldLastName.getStyleClass().add("fieldOk");
             fldFirstName.getStyleClass().add("fieldOk");
             fldEmail.getStyleClass().add("fieldOk");
+             */
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText("Registration is successful");
+            alert.setContentText("You can proceed to login");
+            alert.showAndWait();
             actionLogin();
         }
     }
 
     private boolean checkPassword(String text) {
+        if(text.isBlank()) return false;
         boolean test = true;
+        if(text.trim().length()<5) {
+            containsFiveChars = false;
+            test = false;
+        }
         if(text.toLowerCase().equals(text)) {
-            containsUppercase = false;
+            containsUppercase = false; // mozes skladno ovome mijenjat sadrzaj tooltip-a kako bi korisnik jos bolje znao sta je poslo po zlu, ovako nemaju smisla ova dodjeljivanja
             test = false; // doesn't contain any uppercase
         }
         if(!containsDigit(text)) {
